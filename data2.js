eea31893db0855e156d4958b214dad5fa0f766b2
Ben Chen <idben.chen@gmail.com>
2026-06-18 13:55:04 +0800

    fake data

---
+++ data2.js
@@ -0,0 +1,16 @@
+const results = [
+    {
+        id: "sn002256",
+        acount: "benchen",
+        email: "ben@gmail.com",
+        items: ["筆記本", "鉛筆"],
+        totals: 288
+    },
+    {
+        id: "sn002259",
+        acount: "maychen",
+        email: "may@gmail.com",
+        items: ["小黃瓜", "苦瓜", "大白菜"],
+        totals: 288
+    }
+]
